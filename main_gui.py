895264808bebedf41c7c0d6ea62acc8a3ccebbfd
DionEngels <d.j.engels@student.tue.nl>
2020-09-23 11:26:28 +0200

    Got rid of quit_program

--- main_gui.py
+++ main_gui.py
@@ -149,15 +149,6 @@ def mt_main(name, fitter, frames_split, roi_locations, shared, q):
         shared[9 * result_index:9 * (result_index + 1)] = result[:]
 
 
-# %% Quit
-
-
-def quit_program():
-    global gui
-    gui.destroy()
-    sys.exit(0)
-
-
 # %% Divert errors
 
 
@@ -746,7 +737,7 @@ class FittingPage(tk.Frame):
         button_load_new = ttk.Button(self, text="Load new", command=lambda: self.load_new(parent))
         button_load_new.grid(row=50, column=45, columnspan=2, sticky='EW', padx=PAD_SMALL)
 
-        button_quit = ttk.Button(self, text="Quit", command=quit_program)
+        button_quit = ttk.Button(self, text="Quit", command= lambda: sys.exit(0))
         button_quit.grid(row=50, column=48, columnspan=2, sticky='EW', padx=PAD_SMALL)
 
         for i in range(0, 49):
@@ -1553,7 +1544,7 @@ if __name__ == '__main__':
     gui = MbxPython()
     gui.geometry(str(GUI_WIDTH) + "x" + str(GUI_HEIGHT) + "+" + str(GUI_WIDTH_START) + "+" + str(GUI_HEIGHT_START))
     gui.iconbitmap(getcwd() + "\ico.ico")
-    gui.protocol("WM_DELETE_WINDOW", lambda: quit_program())
+    gui.protocol("WM_DELETE_WINDOW", lambda: sys.exit(0))
 
     ttk_style = ttk.Style(gui)
     ttk_style.configure("Big.TButton", font=FONT_BUTTON_BIG)
