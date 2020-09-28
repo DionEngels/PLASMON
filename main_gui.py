bfc4e4352da9c8a0dbb7c8914a85003d21fe4c6b
DionEngels <d.j.engels@student.tue.nl>
2020-09-28 16:02:34 +0200

    Further improved error messages

--- main_gui.py
+++ main_gui.py
@@ -160,6 +160,8 @@ def show_error_critical(self, exc, val, tb):
 
 def show_error(critical):
     exc_type, exc_value, exc_traceback = sys.exc_info()  # most recent (if any) by default
+    while '_code' in exc_traceback.tb_next.tb_frame.f_globals['__name__']:
+        exc_traceback = exc_traceback.tb_next
     traceback_details = {
         'filename': exc_traceback.tb_frame.f_code.co_filename,
         'lineno': exc_traceback.tb_lineno,
