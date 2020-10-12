da8ecfb1c40517a20521215ee73293a2688c75db
DionEngels <d.j.engels@student.tue.nl>
2020-10-12 10:50:57 +0200

    Continued work on GUI. MainPage no order, but all blocks

--- main_gui.py
+++ main_gui.py
@@ -358,38 +358,110 @@ class MbxPython(tk.Tk):
         self.show_page(MainPage)
 
         self.experiments = []
+        self.list_of_datasets = []
         self.experiment_to_link_name = None
 
     def show_page(self, page):
         frame = self.frames[page]
         frame.tkraise()
 
-# %% Main page
+# %% Main page base
 
 
-class MainPage(tk.Frame):
+class MainPageBase(tk.Frame):
     def __init__(self, container, controller):
         tk.Frame.__init__(self, container)
         self.configure(bg='white')
         self.controller = controller
 
-        button1 = BigButton(self, text="ADD EXPERIMENT", height=int(GUI_HEIGHT / 4),
-                            width=int(GUI_WIDTH / 4),  # style= 'Big.TButton',
-                            command=lambda: self.add_experiment())
-        button1.grid(row=0, column=0)
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=1)
+        label_version = tk.Label(self, text="MBx Python, version: " + __version__, font=FONT_LABEL, bg='white',
+                                 anchor='w')
+        label_version.grid(row=50, column=0, columnspan=20, sticky='EW', padx=PAD_SMALL)
 
-        button2 = BigButton(self, text="ADD DATASET", height=int(GUI_HEIGHT / 4),
-                            width=int(GUI_WIDTH / 4),  # style= 'Big.TButton',
-                            command=lambda: self.add_dataset())
-        button2.grid(row=1, column=0)
+        button_quit = ttk.Button(self, text="Quit", command=lambda: quit_gui(self.controller))
+        button_quit.grid(row=50, column=43, columnspan=6, sticky='EW', padx=PAD_SMALL)
+
+    def column_row_configure(self):
+        for i in range(49):
+            self.grid_columnconfigure(i, weight=1)
+        for i in range(51):
+            self.grid_rowconfigure(i, weight=1)
+
+
+# %% Base page
+
+
+class BasePage(MainPageBase):
+    def __init__(self, container, controller):
+        super().__init__(container, controller)
+
+        button_cancel = ttk.Button(self, text="Cancel", command=lambda: self.cancel())
+        button_cancel.grid(row=50, column=37, columnspan=6, sticky='EW', padx=PAD_SMALL)
+
+    def cancel(self):
+        self.controller.show_page(MainPage)
+
+# %% Main page
+
+
+class MainPage(MainPageBase):
+    def __init__(self, container, controller):
+        super().__init__(container, controller)
+
+        label_new = tk.Label(self, text="New", font=FONT_HEADER, bg='white')
+        label_new.grid(row=0, column=16, columnspan=8, sticky='EW', padx=PAD_SMALL)
+
+        button_new_experiment = BigButton(self, text="ADD EXPERIMENT", height=int(GUI_HEIGHT / 4),
+                                          width=int(GUI_WIDTH / 4), command=lambda: self.add_experiment())
+        button_new_experiment.grid(row=0, column=0)
+
+        button_new_dataset = BigButton(self, text="ADD DATASET", height=int(GUI_HEIGHT / 4),
+                                       width=int(GUI_WIDTH / 4), command=lambda: self.add_dataset())
+        button_new_dataset.grid(row=1, column=0)
+
+        label_loaded = tk.Label(self, text="Loaded", font=FONT_HEADER, bg='white')
+        label_loaded.grid(row=0, column=16, columnspan=8, sticky='EW', padx=PAD_SMALL)
+
+        self.listbox_loaded = tk.Listbox(self)
+        self.listbox_loaded.grid(row=0, column=16, columnspan=8, sticky='EW', padx=PAD_SMALL)
+
+        button_loaded_delete = ttk.Button(self, text="Delete", command=lambda: self.delete_experiment())
+        button_loaded_delete.grid(row=2, column=16, columnspan=8, sticky='EW', padx=PAD_SMALL)
+
+        button_loaded_deselect = ttk.Button(self, text="Delete", command=lambda: self.deselect_experiment())
+        button_loaded_deselect.grid(row=2, column=16, columnspan=8, sticky='EW', padx=PAD_SMALL)
+
+        label_queued = tk.Label(self, text="Queued", font=FONT_HEADER, bg='white')
+        label_queued.grid(row=0, column=16, columnspan=8, sticky='EW', padx=PAD_SMALL)
+
+        self.listbox_queued = tk.Listbox(self)
+        self.listbox_queued.grid(row=0, column=16, columnspan=8, sticky='EW', padx=PAD_SMALL)
+
+        button_run = ttk.Button(self, text="Run", command=lambda: self.run())
+        button_run.grid(row=2, column=16, columnspan=8, sticky='EW', padx=PAD_SMALL)
+
+        label_progress_task = tk.Label(self, text="Task Progress", font=FONT_HEADER, bg='white')
+        label_progress_task.grid(row=0, column=16, columnspan=8, sticky='EW', padx=PAD_SMALL)
+
+        label_progress_overall = tk.Label(self, text="Overall Progress", font=FONT_HEADER, bg='white')
+        label_progress_overall.grid(row=0, column=16, columnspan=8, sticky='EW', padx=PAD_SMALL)
+
+        self.label_progress_task_status = NormalLabel(self, text="Not yet started", bd=1, relief='sunken',
+                                                      row=24, column=45, columnspan=5, sticky="ew", font=FONT_LABEL)
+        self.label_progress_overall_status = NormalLabel(self, text="Not yet started", bd=1, relief='sunken',
+                                                         row=24, column=45, columnspan=5, sticky="ew", font=FONT_LABEL)
+
+        self.label_current_task = NormalLabel(self, text="Not yet started", bd=1, relief='sunken',
+                                              row=24, column=45, columnspan=5, sticky="ew", font=FONT_LABEL)
+
+        label_time_done = tk.Label(self, text="Time done", font=FONT_HEADER, bg='white')
+        label_time_done.grid(row=0, column=16, columnspan=8, sticky='EW', padx=PAD_SMALL)
+
+        self.label_time_done_status = NormalLabel(self, text="Not yet started", bd=1, relief='sunken',
+                                                  row=24, column=45, columnspan=5, sticky="ew", font=FONT_LABEL)
 
         # general setup
-
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_rowconfigure(1, weight=1)
+        self.column_row_configure()
 
     def add_experiment(self):
         self.controller.show_page(LoadPage)
@@ -399,17 +471,24 @@ class MainPage(tk.Frame):
         self.controller.show_page(LoadPage)
         self.controller.experiment_to_link_name = "Test"  # TO DO
 
+    def delete_experiment(self):
+        pass
+
+    def deselect_experiment(self):
+        pass
+
+    def run(self):
+        pass
+
 # %% Loading page
 
 
-class LoadPage(tk.Frame):
+class LoadPage(BasePage):
     """
     Loading page. On this page, there are only two big buttons to select which type of dataset you want to load
     """
     def __init__(self, container, controller):
-        tk.Frame.__init__(self, container)
-        self.configure(bg='white')
-        self.controller = controller
+        super().__init__(container, controller)
 
         button1 = BigButton(self, text="TT", height=int(GUI_HEIGHT / 4),
                             width=int(GUI_WIDTH / 4),  # style= 'Big.TButton',
@@ -424,10 +503,7 @@ class LoadPage(tk.Frame):
         button2.grid(row=0, column=1)
 
         # general setup
-
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=1)
+        self.column_row_configure()
 
     def load_nd2(self, dataset_type):
         filename = askopenfilename(filetypes=FILETYPES,
@@ -455,29 +531,23 @@ class LoadPage(tk.Frame):
 # %% ROIPage
 
 
-class ROIPage(tk.Frame):
+class ROIPage(BasePage):
     def __init__(self, container, controller):
-        tk.Frame.__init__(self, container)
-        self.configure(bg='white')
-        self.controller = controller
+        super().__init__(container, controller)
 
 # %% TTPage
 
 
-class TTPage(tk.Frame):
+class TTPage(BasePage):
     def __init__(self, container, controller):
-        tk.Frame.__init__(self, container)
-        self.configure(bg='white')
-        self.controller = controller
+        super().__init__(container, controller)
 
 # %% HSMPage
 
 
-class HSMPage(tk.Frame):
+class HSMPage(BasePage):
     def __init__(self, container, controller):
-        tk.Frame.__init__(self, container)
-        self.configure(bg='white')
-        self.controller = controller
+        super().__init__(container, controller)
 
 # %% START GUI and declare styles (how things look)
 
