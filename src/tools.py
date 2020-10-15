de5e6d6dc5bb36ee2fbd490d3b690cca3bb36cf6
DionEngels <d.j.engels@student.tue.nl>
2020-10-15 15:29:57 +0200

    Documentation of tools.py and fix for ROI index

--- src/tools.py
+++ src/tools.py
@@ -23,6 +23,7 @@ v0.8: metadata v3
 v1.0: bug fixes
 v1.0.1: metadata goldmine found, to be implemented
 v1.1: emptied out: 09/08/2020
+v2.0: part of GUI v2: 15/10/2020
 
 """
 from numpy import zeros, moveaxis
@@ -33,17 +34,11 @@ __self_made__ = True
 def change_to_nm(results, metadata, method):
     """
     Change the pixels to nm
-
-    Parameters
     -----------------
-    results: the results in pixels
-    metadata: metadata to get the pixelsize from
-    method: method, to check if sigmas also need to be changed
-
-    Returns
-    ------------------
-    results: the results in nm
-
+    :param: results: the results in pixels
+    :param: metadata: metadata to get the pixelsize from
+    :param: method: method, to check if sigmas also need to be changed
+    :return: results: the results in nm
     """
     pixelsize_nm = metadata['pixel_microns'] * 1000
     results[:, 1] *= pixelsize_nm  # y position to nm
@@ -56,63 +51,67 @@ def change_to_nm(results, metadata, method):
 
 
 def convert_to_matlab(experiment):
-
+    """
+    Converts an experiments results to MATLAB
+    -----------------------
+    :param experiment: experiment to convert
+    :return: None. Changes experiment object
+    """
     for dataset in experiment.datasets:
+        # set offset to MATLAB
         dataset.roi_offset = offset_to_matlab(dataset.roi_offset)
         for roi in dataset.active_rois:
             if dataset.type == "TT":
+                # Change results to matlab, both pre- and post-drift
                 roi.results[dataset.name_result]['result'] = \
                     result_to_matlab(roi.results[dataset.name_result]['result'], dataset.settings['method'])
                 roi.results[dataset.name_result]['result_post_drift'] = \
                     result_to_matlab(roi.results[dataset.name_result]['result_post_drift'], dataset.settings['method'])
+                # change drift and raw to matlab
                 roi.results[dataset.name_result]['drift'] = switch_axis(roi.results[ dataset.name_result]['drift'])
                 roi.results[ dataset.name_result]['raw'] = raw_to_matlab(roi.results[ dataset.name_result]['raw'])
             elif dataset.type == "HSM":
+                # change HSM to matalb
                 roi.results[ dataset.name_result]['raw'] = raw_to_matlab(roi.results[ dataset.name_result]['raw'])
             else:
                 pass
 
-    for roi in experiment.rois:
-        roi.y, roi.index = roi_to_matlab(roi, experiment.frame_for_rois.shape[0])
+    # change ROIs to matlab
+    experiment.rois = roi_to_matlab(experiment.rois)
 
 
 def offset_to_matlab(offset):
+    """
+    Change offset to MATLAB
+    ---------------------
+    :param offset: base offet
+    :return: MATLAB offset
+    """
     new = offset
     new[1] = offset[0]
     new[0] = offset[1]
     return new
 
 
-def roi_to_matlab(roi, height):
+def roi_to_matlab(rois):
     """
-    Change from Python to MATLAB coordinates for ROI locations
-
-    Parameters
-    -----------------
-    roi: a single ROI
-    height: Height of microscope view
-
-    Returns
-    ------------------
-    results: the roi.y coordinate switched to MATLAB coordinates
+    ROIs to MATLAB. Adds one to index
+    --------------------------
+    :param rois: All ROIs in experiment
+    :return: New ROIs
     """
-    roi.y = height - roi.y
-
-    return roi.y, roi.index + 1
+    for roi in rois:
+        roi.index += 1
+    return rois
 
 
 def result_to_matlab(results, method):
     """
     Change from Python to MATLAB coordinates for results
-
-    Parameters
     -----------------
-    results: the results in Python coordinates
-    method: to check whether or not sigmas also need to be changed
-
-    Returns
-    ------------------
-    results: the results switched to MATLAB coordinates
+    :param: results: the results in Python coordinates
+    :param: method: to check whether or not sigmas also need to be changed
+    :return: results: the results switched to MATLAB coordinates
     """
     results[:, 0] += 1  # add one to frame counting
 
@@ -125,6 +124,12 @@ def result_to_matlab(results, method):
 
 
 def raw_to_matlab(raw):
+    """
+    Switch axis of raw for MATLAB
+    -----------------------
+    :param raw: raw data
+    :return: new raw data
+    """
     if raw.ndim > 2:
         raw = moveaxis(raw, 0, -1)
 
@@ -134,15 +139,9 @@ def raw_to_matlab(raw):
 def switch_axis(array):
     """
     Switches a single arrays values
-
-    Parameters
     ----------
-    array : array to switch
-
-    Returns
-    -------
-    new : switched array
-
+    :param: array : array to switch
+    :return: new : switched array
     """
     new = zeros(array.shape)
     new[:, 1] = array[:, 0]
