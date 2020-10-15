3682fcc9b3362215d5cb33a99ce009600d316a46
DionEngels <d.j.engels@student.tue.nl>
2020-10-15 14:29:26 +0200

    Documentation of figure_making.py

--- src/figure_making.py
+++ src/figure_making.py
@@ -15,23 +15,21 @@ v1.0: split from tools: 07/08/2020
 v1.1: individual ROI figures
 v1.2: minor improvement based on Sjoerd's feedback: 27/08/2020
 v1.3: feedback of Peter meeting: 06/09/2020
+v2.0: Figure making for v2.0 of GUI: 15/10/2020
 
 """
 
 from os import mkdir
-from numpy import asarray, invert, concatenate, linspace, nanmin, nanmax
+from numpy import invert, linspace, nanmin, nanmax
 from numpy import max as np_max
 from numpy import min as np_min
 from math import ceil, floor
-
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from matplotlib.lines import Line2D
 from matplotlib.gridspec import GridSpec
-
-import _thread
 
 __self_made__ = True
 DPI = 400
@@ -40,8 +38,7 @@ N_TICKS = 4
 
 def plot_rois(ax, frame, roi_locations=None, roi_size=None, roi_offset=None, font_size=None):
     """
-
-    Parameters
+    Plots a microscope with rois if deisred
     ----------
     :param ax: axis to plot to
     :param frame : frame to plot
@@ -49,32 +46,41 @@ def plot_rois(ax, frame, roi_locations=None, roi_size=None, roi_offset=None, fon
     :param roi_size : Size of boxes to draw
     :param roi_offset: offset of the dataset compared to experiment ROIs
     :param font_size: size of font of roi number
-
-    Returns
-    -------
-    None.
-
+    :returns None. Edits ax object
     """
     if roi_offset is None:
-        roi_offset = [0, 0]
+        roi_offset = [0, 0]  # if no offset given, set to 0
+
+    # show frame
     ax.imshow(frame, extent=[0, frame.shape[1], frame.shape[0], 0], aspect='auto')
+
+    # add ROIs if not None
     if roi_locations is not None and roi_size is not None:
         roi_size_1d = int((roi_size - 1) / 2)
         for roi in roi_locations:
-
+            # red square for each ROI
             rect = patches.Rectangle((roi.x - roi_size_1d + roi_offset[1], roi.y + roi_offset[0] - roi_size_1d),
                                      roi_size, roi_size, linewidth=0.5, edgecolor='r', facecolor='none')
             ax.add_patch(rect)
             if font_size is not None:
+                # ROI number
                 ax.text(roi.x + roi_offset[1] - roi_size_1d, roi.y + roi_offset[0] - roi_size_1d, str(roi.index + 1),
                         color='red', fontsize=font_size)
 
 
 def find_range(dataset_name, rois):
+    """
+    Finds the range of all scatter plots
+    ----------------------
+    :param dataset_name: Name of the dataset to get range for
+    :param rois: All the ROIs
+    :return: max_range: the highest range, will afterwards be used for all datasets
+    """
 
     max_range = 0
 
     for roi in rois:
+        # get results per ROI and find max_range of ROI. If larger than before, save.
         results = roi.results[dataset_name]['result']
         y_positions = results[:, 1]
         x_positions = results[:, 2]
@@ -84,6 +90,7 @@ def find_range(dataset_name, rois):
         if nanmax(y_positions) - nanmin(y_positions) > max_range:
             max_range = nanmax(y_positions) - nanmin(y_positions)
 
+    # settle on nice round number
     if max_range > 999:
         max_range = ceil(max_range / 100) * 100
     elif max_range > 199:
@@ -97,18 +104,37 @@ def find_range(dataset_name, rois):
 
 
 def plot_hsm(ax, result, wavelengths):
+    """
+    Plotter of a HSM result
+    ----------------------
+    :param ax: ax object to edit
+    :param result: result to plot
+    :param wavelengths: wavelengths used to get result. Will become x-axis
+    :return: None. Edits ax object
+    """
     def lorentzian(params, x):
+        """
+        Lorentzian formula. Taken from SPectrA
+        ----------------
+        :param params: Parameters of lorentzian. Need to be four.
+        :param x: x-axis. Wavelengths
+        :return: array of values for current parameters and wavelengths
+        """
         return params[0] + params[1] / ((x - params[2]) ** 2 + (0.5 * params[3]) ** 2)
 
+    # scatter plot of actual results
     ax.scatter(wavelengths, result['intensity'])
     try:
+        # try to fit and plot fit over
         wavelengths_ev = 1248 / linspace(np_min(wavelengths), np_max(wavelengths), num=50)
         hsm_fit = lorentzian(result['fit_parameters'], wavelengths_ev)
         ax.plot(1248 / wavelengths_ev, hsm_fit, 'r--')
     except:
         pass
+    # set axis to same range every time
     ax.set_xlim(np_min(wavelengths) - 30, np_max(wavelengths) + 30)
     try:
+        # add fit info if possible
         text = '\n'.join((r'SPR (nm)=%.1f' % (result['result'][0],),
                           r'linewidth (meV)=%.1f' % (result['result'][1],),
                           r'r^2=%.1f' % (result['result'][2],)))
@@ -119,25 +145,39 @@ def plot_hsm(ax, result, wavelengths):
 
 
 def make_tt_scatter(ax, result, event_or_not_boolean, dataset):
-    ax.clear()
+    """
+    Scatter plot of TT results
+    -------------------------
+    :param ax: ax object to edit
+    :param result: results to plot
+    :param event_or_not_boolean: boolean that tells whether or not each time step is event or not
+    :param dataset: dataset object from which current results are obtained
+    :return: None. Edits ax object
+    """
+    ax.clear()  # clear to be sure
+    # plot events and non-events
     y_pos = result[invert(event_or_not_boolean), 1]
     x_pos = result[invert(event_or_not_boolean), 2]
     y_pos_event = result[event_or_not_boolean, 1]
     x_pos_event = result[event_or_not_boolean, 2]
 
+    # scatter plot and set colours
     if len(y_pos_event) != 0:
         ax.scatter(x_pos_event, y_pos_event, color='red')
     if len(y_pos) != 0:
         ax.scatter(x_pos, y_pos, color='#1f77b4')
 
+    # force same legend
     legend_events = Line2D([0], [0], marker='o', color='w', label='events',
                            markerfacecolor='red', markersize=10)
     legend_non_events = Line2D([0], [0], marker='o', color='w', label='non-events',
                                markerfacecolor='#1f77b4', markersize=10)
 
+    # set square axis and put in legend
     ax.axis('equal')
     ax.legend(handles=[legend_events, legend_non_events])
 
+    # set labels, range, and ticks
     if dataset.settings['pixels_or_nm'] == 'nm':
         ax.set_xlabel('x-position (nm)')
         ax.set_ylabel('y-position (nm)')
@@ -147,11 +187,43 @@ def make_tt_scatter(ax, result, event_or_not_boolean, dataset):
     set_range_and_ticks(ax, dataset.figure_range)
 
 
-def make_tt(ax, time_axis, result, method, roi_index):
-    linewidth = 1 / (2 ** (floor(len(time_axis) / 1500) - 1))
+def set_range_and_ticks(ax, max_range):
+    """
+    Sets range and ticks for each scatter plot
+    --------------------------------------
+    :param ax: ax object to edit
+    :param max_range: max range to put scatter too
+    :return: None. Edits ax object
+    """
+    # get x and y range
+    y_min, y_max = ax.get_ylim()
+    x_min, x_max = ax.get_xlim()
+    # set new x and y ranges
+    y_center = (y_max + y_min) / 2
+    x_center = (x_max + x_min) / 2
+    ax.set_xlim(x_center - max_range / 2, x_center + max_range / 2)
+    ax.set_ylim(y_center - max_range / 2, y_center + max_range / 2)
+    # set ticks
+    ax.xaxis.set_major_locator(plt.MaxNLocator(N_TICKS))
+    ax.yaxis.set_major_locator(plt.MaxNLocator(N_TICKS))
+
+
+def make_tt(ax, time_axis, result, method):
+    """
+    Makes line plot of TT
+    --------------------
+    :param ax: ax object to edit
+    :param time_axis: time axis, the x-axis of the plot
+    :param result: results to plot
+    :param method: method used to get results. Impacts labels
+    :return: None. Edits ax object
+    """
+    linewidth = 1 / (2 ** (floor(len(time_axis) / 1500) - 1))  # variable linewidth depending on length result
+    # plot resulting intensities
     intensities = result[:, 3]
     ax.plot(time_axis, intensities, linewidth=linewidth)
 
+    # put in label depending on method
     if "Gaussian" in method:
         ax.set_xlabel('Time (s)')
         ax.set_ylabel('Integrated intensity (counts)')
@@ -160,25 +232,24 @@ def make_tt(ax, time_axis, result, method, roi_index):
         ax.set_ylabel('Summed intensity (counts)')
 
 
-def set_range_and_ticks(ax, max_range):
-    y_min, y_max = ax.get_ylim()
-    x_min, x_max = ax.get_xlim()
-    y_center = (y_max + y_min) / 2
-    x_center = (x_max + x_min) / 2
-    ax.set_xlim(x_center - max_range / 2, x_center + max_range / 2)
-    ax.set_ylim(y_center - max_range / 2, y_center + max_range / 2)
-    ax.xaxis.set_major_locator(plt.MaxNLocator(N_TICKS))
-    ax.yaxis.set_major_locator(plt.MaxNLocator(N_TICKS))
-
-
 def save_overview(experiment):
+    """
+    Makes overview figure of experiment
+    ---------------------------------
+    :param experiment: Experiment to make overview figure of
+    :return: None. Saves figure to disk
+    """
+    # force agg backend. Otherwise breaks due to threading
     mpl.use('agg', force=True)
     from matplotlib import pyplot as plt
+    # set title size to small for all figures
     mpl.rcParams['axes.titlesize'] = 'small'
 
+    # make graphs directory
     path = experiment.directory + "/Graphs"
     mkdir(path)
 
+    # determine number of datasets in experiment and required figure size
     figure_length_base = 6
     tt = []
     hsm = []
@@ -192,31 +263,37 @@ def save_overview(experiment):
     per_roi_length = len(tt) + max(len(hsm), 1)
     total_length = figure_length_base + 2 * per_roi_length
 
+    # make figure and grid in figure
     fig = plt.figure(constrained_layout=True, figsize=(16, total_length*4), dpi=DPI)
     widths = [1] * 4
     heights = [1] * total_length
     gs = GridSpec(total_length, 4, figure=fig, width_ratios=widths, height_ratios=heights)
+    # create base figures, overview, sigma histogram, and intensity histogram
     ax_overview = fig.add_subplot(gs[0:4, :])
     ax_sigma = fig.add_subplot(gs[4:6, :2])
     ax_int = fig.add_subplot(gs[4:6, 2:])
 
+    # plot ROIs to overview
     plot_rois(ax_overview, experiment.frame_for_rois, roi_locations=experiment.rois, roi_size=7, font_size='large')
     ax_overview.set_xlabel('x (pixels)')
     ax_overview.set_ylabel('y (pixels)')
     ax_overview.set_title('Full first frame')
 
+    # Histogram of intensities
     ax_int.hist(experiment.roi_finder.int_list, bins=100)
     ax_int.set_xlabel('Integrated intensity (counts)')
     ax_int.set_ylabel('Occurrence')
     ax_int.set_title('Integrated intensity occurrence')
 
+    # Histogram of sigma
     ax_sigma.hist(experiment.roi_finder.sigma_list, bins=50)
     ax_sigma.set_xlabel('Sigma (pixels)')
     ax_sigma.set_ylabel('Occurrence')
     ax_sigma.set_title('Sigmas occurrence')
 
+    # sample ROIs to put in overview
     roi_list = []
-    if len(experiment.rois) > 3:
+    if len(experiment.rois) > 3:  # if 4 or more, just take those using modulo
         for roi in experiment.rois:
             if roi.index % int(len(experiment.rois) / 3) == 0:
                 roi_list.append(roi)
@@ -225,14 +302,16 @@ def save_overview(experiment):
         while len(roi_list) > 4:
             roi_list.pop()
     else:
-        for i in range(4):
+        for i in range(4): # otherwise, double sample. Always get four
             value = i % len(experiment.rois)
             roi_list.append(experiment.rois[value])
 
+    # for each ROI, plot all datasets
     for n_roi, roi in enumerate(roi_list):
         row = figure_length_base + int(floor(n_roi / 2)) * per_roi_length
         column = (n_roi % 2) * 2
 
+        # first zoomed in frame
         ax_frame = fig.add_subplot(gs[row, column])
         plot_rois(ax_frame, roi.get_roi(experiment.frame_for_rois, 7, [0, 0]))
         ax_frame.set_xlabel('x (pixels)')
@@ -240,6 +319,7 @@ def save_overview(experiment):
         ax_frame.set_title('Zoom-in ROI {}'.format(roi.index + 1))
 
         for index_dataset, n_dataset in enumerate(hsm):
+            # then each HSM
             ax_hsm = fig.add_subplot(gs[row + index_dataset, column + 1])
             plot_hsm(ax_hsm, roi.results[experiment.datasets[n_dataset].name_result],
                      experiment.datasets[n_dataset].wavelengths)
@@ -248,6 +328,7 @@ def save_overview(experiment):
             ax_hsm.set_title('HSM {} ROI {}'.format(experiment.datasets[n_dataset].name, roi.index + 1))
 
         for index_dataset, n_dataset in enumerate(tt):
+            # then each TT, first scatter
             method = experiment.datasets[n_dataset].settings['method']
             ax_tt_scatter = fig.add_subplot(gs[row + max(len(hsm), 1) + index_dataset, column])
             make_tt_scatter(ax_tt_scatter, roi.results[experiment.datasets[n_dataset].name_result]['result_post_drift'],
@@ -257,11 +338,13 @@ def save_overview(experiment):
                                                                              roi.index + 1))
 
             if "Gaussian" in method or "Sum" in method:
+                # and if possible, time trace
                 ax_tt = fig.add_subplot(gs[row + max(len(hsm), 1) + index_dataset, column + 1])
                 make_tt(ax_tt, experiment.datasets[n_dataset].time_axis,
                         roi.results[experiment.datasets[n_dataset].name_result]['result'], method, roi.index)
                 ax_tt.set_title('TT {} ROI {}'.format(experiment.datasets[n_dataset].name, roi.index + 1))
 
+    # save
     name = path + "/" + "_Overview.png"
     plt.tight_layout()
     fig.savefig(name, bbox_inches='tight')
@@ -270,12 +353,22 @@ def save_overview(experiment):
 
 
 def individual_figures(experiment):
+    """
+    Makes individual figure of experiment
+    ---------------------------------
+    :param experiment: Experiment to make overview figure of
+    :return: None. Saves figures to disk
+    """
+    # force agg backend. Otherwise breaks due to threading
     mpl.use('agg', force=True)
     from matplotlib import pyplot as plt
+    # set title size to small for all figures
     mpl.rcParams['axes.titlesize'] = 'small'
 
+    # set path, already created by overview
     path = experiment.directory + "/Graphs"
 
+    # determine number of datasets in experiment and required figure size
     tt = []
     hsm = []
     for n_dataset, dataset in enumerate(experiment.datasets):
@@ -286,9 +379,11 @@ def individual_figures(experiment):
 
     per_roi_length = len(tt) + max(len(hsm), 1)
 
+    # for each ROI, plot all datasets
     for roi in experiment.rois:
         fig = plt.figure(figsize=(8, 4*per_roi_length), dpi=DPI)
 
+        # first zoomed in frame
         ax_frame = fig.add_subplot(per_roi_length, 2, 1)
         plot_rois(ax_frame, roi.get_roi(experiment.frame_for_rois, 7, [0, 0]))
         ax_frame.set_xlabel('x (pixels)')
@@ -296,6 +391,7 @@ def individual_figures(experiment):
         ax_frame.set_title('Zoom-in ROI {}'.format(roi.index + 1))
 
         for index_dataset, n_dataset in enumerate(hsm):
+            # then each HSM
             ax_hsm = fig.add_subplot(per_roi_length, 2, 2 + index_dataset * 2)
             plot_hsm(ax_hsm, roi.results[experiment.datasets[n_dataset].name_result],
                      experiment.datasets[n_dataset].wavelengths)
@@ -304,6 +400,7 @@ def individual_figures(experiment):
             ax_hsm.set_title('HSM {} ROI {}'.format(experiment.datasets[n_dataset].name, str(roi.index + 1)))
 
         for index_dataset, n_dataset in enumerate(tt):
+            # then each time trace scatter
             method = experiment.datasets[n_dataset].settings['method']
             ax_tt_scatter = fig.add_subplot(per_roi_length, 2, 1 + index_dataset * 2 + max(len(hsm), 1) * 2)
             make_tt_scatter(ax_tt_scatter, roi.results[experiment.datasets[n_dataset].name_result]['result_post_drift'],
@@ -313,11 +410,13 @@ def individual_figures(experiment):
                                                                              roi.index + 1))
 
             if "Gaussian" in method or "Sum" in method:
+                # and if possible, time trace
                 ax_tt = fig.add_subplot(per_roi_length, 2, 2 + index_dataset * 2 + max(len(hsm), 1) * 2)
                 make_tt(ax_tt, experiment.datasets[n_dataset].time_axis,
                         roi.results[experiment.datasets[n_dataset].name_result]['result'], method, roi.index)
                 ax_tt.set_title('TT {} ROI {}'.format(experiment.datasets[n_dataset].name, roi.index + 1))
 
+        # save
         name = path + "/" + "ROI" + str(roi.index+1)+".png"
         plt.tight_layout()
         fig.savefig(name, bbox_inches='tight')
