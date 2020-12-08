9b4c950bdde3e9698a6e83fc7e8c779248d61f4e
DionEngels <d.j.engels@studennt.tue.nl>
2020-12-08 09:07:30 +0100

    New build location

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ from cx_Freeze import setup, Executable
 
 __self_made__ = True
 
-r'-b C:\Users\s150127\Downloads\___MBx\build'  # take this control location of build
+r'-b C:\Users\dione\Documents\___MBx\build'  # take this control location of build
 
 __version__ = '2.1.2'
 
