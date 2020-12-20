63938732659326292e77a1f2d3af9e948c04f2b8
DionEngels <d.j.engels@studennt.tue.nl>
2020-12-20 11:00:43 +0100

    Remove test codes

--- test codes/cython_test/pelles_c/pelles_c_test/demolib.h
+++
@@ -1,3 +0,0 @@
-unsigned long cfactorial_sum(char num_chars[]);
-unsigned long ifactorial_sum(long nums[], int size);
-unsigned long factorial(long n);
--- test codes/cython_test/pelles_c/pelles_c_test/setup.py
+++
@@ -1,4 +0,0 @@
-from setuptools import Extension, setup
-
-module = Extension("demo", sources=['demolib.c', 'demomodule.c'])
-setup(name='demo', version='1.0', description='Python wrapper for custom C extension', ext_modules=[module])
