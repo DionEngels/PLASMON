48bf155df5523767b2eddad866e808f3bf7c5c38
DionEngels <d.j.engels@studennt.tue.nl>
2020-12-20 12:01:42 +0100

    Compile to .exe

--- runners/PLASMON_with_terminal.bat
+++ runners/PLASMON_with_terminal.bat
@@ -1,3 +1,36 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFB9dQwqHAES0A5EO4f7+r6fHlnhdcesxfIfUzLGAHNcW+E7YWLQ1lkpDjMMDAltZcBOndxw9ulJhuWCAC86fvEHoSUfp
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFB9dQwqHAES0A5EO4f7+r6fHlnhdcesxfIfUzLGAHNcW+E7YWLQ1lkpDjMMDAltZcBOndxw9ulJhuWCAC/ewkjzMZWa28UIkE1pggnHdhSU6bJ1tgsZj
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
 :PythonCheck
 python -V >NUL
