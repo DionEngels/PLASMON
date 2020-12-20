1959cf088aacb5b5e6204ecc647ec8e55734ec90
DionEngels <d.j.engels@studennt.tue.nl>
2020-12-20 12:00:22 +0100

    Move .bats

--- runners/PLASMON_installer.bat
+++ runners/PLASMON_installer.bat
@@ -1,3 +1,36 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFB9dQwqHAES0A5EO4f7+r6fHlnhdcesxfIfUzLGAHNcW+E7YWLQ1lkpDjMMDAltZcBOndxw9ulJhuWCAC86fvEHoSUfp
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdFe5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVsGAlbMbAs=
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFB9dQwqHAES0A5EO4f7+r6fHlnhdcesxfIfUzLGAHNcW+E7YWLQ1lkpDjMMDAltZcBOndxw9ulJhuWCAC/ewkjzMZWa270UjD2R4i2bCwi4jZbM=
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
 set python_version=3.8.6
 set python_url=https://www.python.org/ftp/python/%python_version%/python-%python_version%.exe
