6a1a7de5f6ec67b0f689fd48c352afac86a0adae
DionEngels <d.j.engels@studennt.tue.nl>
2020-12-20 11:46:06 +0100

    Fix terminal script

--- PLASMON_with_terminal.bat
+++ PLASMON_with_terminal.bat
@@ -3,7 +3,7 @@
 python -V >NUL
 if %errorLevel% == 0 (
 	set py_call=python
-	goto Clone
+	goto Run
 ) else (
 	goto PythonCheck2
 )
@@ -12,28 +12,19 @@ if %errorLevel% == 0 (
 py -V >NUL
 if %errorLevel% == 0 (
 	set py_call=py
-	goto PythonCheckDir
-) else (
-	echo Failure: You don't have Python installed. Run the installer script.
-	exit /b 1
-) 
-
-
-:PythonCheckDir
-for /f %%i in ('%py_call% -c "import sys; import os; print(os.path.dirname(sys.executable))"') do set py_dir=%%i
-if %errorLevel% == 0 (
 	goto Run
 ) else (
-	echo Failure: Cannot find python installation dir. Please contact administrator.
+	echo Failure: You don't have Python installed. Run the installer script.
+	pause
 	exit /b 1
 ) 
 
 :Run
-%py_dir%\PLASMON_venv\Scripts\activate.bat & %py_call% main_gui.py & exit
-exit
+%cd%\venv\Scripts\activate.bat & %py_call% main_gui.py & exit /b 0
 if %errorLevel% == 0 (
 	exit /b 0
 ) else (
-	echo Failure: Cannot run program. Contact administrator.
+	echo Failure: Cannot run program. Rerun your installer if you touched the venv folder. Otherwise contact administrator.
+	pause
 	exit /b 1
 ) 
